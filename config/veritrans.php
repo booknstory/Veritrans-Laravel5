64985b00cf248b09b882be7179d72bb925afe0ef
Book and Story <tester.booknstory@gmail.com>
2016-08-27 12:07:37 +0700

    Add config file

---
+++ config/veritrans.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Veritrans Config
+    |--------------------------------------------------------------------------
+    |
+    | Change values according to your credentials.
+    |
+    */
+    'serverKey'    => null,  // fill with your own Server Key
+    'isProduction' => false, // turn "true" for for Production, and "false" for Sandbox
+];
